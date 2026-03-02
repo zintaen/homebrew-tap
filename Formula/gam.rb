dff69c4c06232a975225aabfc220590b0be8c009
Stephen Cheng <zintaen@gmail.com>
2026-03-02 23:11:23 +0700

    brew: update gam to v1.0.5

--- Formula/gam.rb
+++ Formula/gam.rb
@@ -1,9 +1,9 @@
 class Gam < Formula
   desc "Git Alias Manager — A professional desktop GUI for managing Git aliases"
   homepage "https://github.com/zintaen/gam"
-  url "https://github.com/zintaen/gam/releases/download/v1.0.3/GAM_1.0.3_aarch64.dmg"
-  sha256 "a4dc6f8bdab6efc7182ba215cc1ef655c77c87ccde841c406d13dc84e4d57499"
-  version "1.0.3"
+  url "https://github.com/zintaen/gam/releases/download/v1.0.5/GAM_1.0.5_aarch64.dmg"
+  sha256 "10d8a0f3be99f3ce65eb7fad7b55e529d52739afef5780daf7fb0831366bcb16"
+  version "1.0.5"
 
   depends_on macos: :high_sierra
 
